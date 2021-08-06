e29361c5ffe0bf59822371a53b77404ee32a8444
Alexander <sasha.kudasov04@gmail.com>
2021-08-06 11:19:11 +0500

    /check

--- src/bot/commands.rs
+++ src/bot/commands.rs
@@ -2,40 +2,46 @@ use super::*;
 use rand::seq::IteratorRandom;
 
 impl Bot {
-    pub fn select(&mut self) -> Response {
+    fn check(&mut self) -> Response {
         let chat_id = self.config.main_chat;
-        let user_count = async_std::task::block_on(
-            self.api()
-                .send(GetChatMembersCount::new(chat_id)),
-        )
-        .unwrap();
+        let user_count =
+            async_std::task::block_on(self.api().send(GetChatMembersCount::new(chat_id))).unwrap();
         let known_count = self.get_users_count() as i64;
-        let mut response = if user_count == known_count {
-            format!("/select was called succefully. ")
-        } else {
-            format!(
-                "/select was called, but chat users info is not relevant. There are {} users in the chat, but only {} are known. ", 
-                user_count, 
-                known_count
-            )
-        };
+        Some(format!(
+            "{} out of {} users are known",
+            known_count, user_count,
+        ))
+    }
 
+    fn select(&mut self) -> Response {
         let mut users = self.get_active_users();
         if users.len() == 0 {
             self.reset_chosen_users();
             users = self.get_active_users();
         }
-        let random_user = users.iter().choose(&mut rand::thread_rng()).unwrap().clone();
-        if self.choose_active_user( random_user.clone()) {
-            response.push_str(&format!("{} has been chosen!", random_user.name))
-        }
+        let random_user = users
+            .iter()
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+            .clone();
         self.queue_save_sheets = true;
-        Some(response)
+        if self.choose_active_user(random_user.clone()) {
+            Some(format!("{} has been chosen!", random_user.name))
+        } else {
+            None
+        }
     }
 }
 
 pub fn bot_commands() -> Commands<Bot, ChatUser> {
     Commands::new(vec![
+        CommandNode::Literal {
+            literals: vec!["/check".to_owned()],
+            child_nodes: vec![CommandNode::Final {
+                authority_level: UserAuthorityLevel::RandomUser as usize,
+                command: Arc::new(|bot, _, _| bot.check()),
+            }],
+        },
         CommandNode::Literal {
             literals: vec!["/select".to_owned()],
             child_nodes: vec![CommandNode::Final {
@@ -58,10 +64,14 @@ pub fn bot_commands() -> Commands<Bot, ChatUser> {
             child_nodes: vec![CommandNode::Final {
                 authority_level: UserAuthorityLevel::RandomUser as usize,
                 command: Arc::new(|bot, sender, _| {
-                    Some(format!("@{}, your authority level is {:?}", sender.name, bot.get_user_authority_level(&sender)))
-                })
-            }]
-        }
+                    Some(format!(
+                        "@{}, your authority level is {:?}",
+                        sender.name,
+                        bot.get_user_authority_level(&sender)
+                    ))
+                }),
+            }],
+        },
     ])
 }
 
